1eff4d20858f275cd9f9a805c68af37fc0034d50
MarkétaMurínová <murinova@skautka.cz>
2024-05-13 10:43:21 +0200

    commit1

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function(eleventyConfig) {
+  // Výchozí výstupní složka je: _site
+  // Zkopírovat images/ do _site/images
+  eleventyConfig.addPassthroughCopy("images");
+  // Zkopírovat css/ to _site/css/
+  eleventyConfig.addPassthroughCopy("css");
+ };
+  // ...necháme, co už v konfiguraci je, a přidáme:
+  return {
+    // možné formáty šablon
+    templateFormats: ["njk", "html", "md"],
+    // jako šablonovací jazyk zvolíme Nunjucks`
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  };
+ 
